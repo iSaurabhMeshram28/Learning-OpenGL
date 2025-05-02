d1653a95bfe555c46f9dde02f8b16e2ba215e85d
Saurabh Meshram <isaurabh.meshram2807@gmail.com>
2025-05-02 12:28:02 +0530

    minor Changes

--- 05-Pyramid-Rotation/OGL.cpp
+++ 05-Pyramid-Rotation/OGL.cpp
@@ -316,8 +316,8 @@ int OpenGLApp::initialize(HWND hwnd)
 // Sets up shaders for rendering
 void OpenGLApp::setupShaders()
 {
-    string vertexShaderSource = readShaderSource("vertexShader.vs");
-    string fragmentShaderSource = readShaderSource("fragmentShader.fs");
+    string vertexShaderSource = readShaderSource("vertexShader.vert");
+    string fragmentShaderSource = readShaderSource("fragmentShader.frag");
 
     if (vertexShaderSource.empty() || fragmentShaderSource.empty())
     {
